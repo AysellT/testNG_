705221896e1b148685e9b177e3889ab9a422227e
AysellT <aysltrg3@gmail.com>
2023-08-25 08:01:42 +0300

    updated

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 public class Driver {
@@ -17,40 +19,55 @@ public class Driver {
     access modifier ini PRIVATE yapmamaiz yeterli olur
      */
     private Driver(){
+
     }
     static WebDriver driver;
+
     static ChromeOptions ops = new ChromeOptions();
-    public static WebDriver getDriver(){
-        String browser = ConfigurationReader.getProperty("browser");
-        if (driver==null) {
-            switch (browser) {
-                case "chrome" :
-                    ops.addArguments("--remote-allow-origins=*","ignore-certificate-errors");
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver(ops);
-                    break;
-                case "firefox" :
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                default :
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            switch (ConfigurationReader.getProperty("browser")) {
+                case "chrome":
+                    ops.addArguments("--remote-allow-origins=*");
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
+                    break;
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    break;
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                default:
+                    ops.addArguments("--remote-allow-origins=*");
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(ops);
+
+
             }
-            //   driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
         return driver;
     }
-    public static void quitDriver(){
-        if (driver != null){
-            driver.quit();
-            driver=null;
-        }
-    }
-    public static void closeDriver(){
-        if (driver != null){
+
+    public static void closeDriver() {
+        if (driver != null) {
             driver.close();
-            driver=null;
+            driver = null;
         }
     }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.close();
+            driver = null;
+        }
+    }
+
 }
